f13a98d1840615805b74f86ce43f26100b5437a5
JoshuaTian-McGill <56935023+JoshuaTian-McGill@users.noreply.github.com>
2021-01-05 12:25:50 -0500

    Add files via upload

---
+++ R/Add.R
@@ -0,0 +1,11 @@
+#' Add two numbers
+#' 
+#' Takes in two numbers to add up
+#' @param x is a numeric value
+#' @param y is a numeric value
+#' @return The sum of the 2 inputs
+#' @export
+
+Add <- function(x, y){
+  return(x + y)
+}
